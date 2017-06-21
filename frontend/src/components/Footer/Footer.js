1a640b723fb787e7cb44dc35914e7ac030b1d832
tiezo <autechregescom@gmail.com>
2017-06-22 00:01:44 +0300

    missing files

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import './Footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="container footer">
+                <a href="https://github.com/tiezo/sere">Source code</a>
+            </footer>
+        );
+    }
+}
